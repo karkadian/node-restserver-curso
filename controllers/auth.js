d7de6590b6dfea930124da052a6f535e5f5bb862
Kevin Carcache <karkadian@hotmail.es>
2022-03-08 12:34:58 -0500

    google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generarJWT");
+const req = require("express/lib/request");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -48,9 +50,55 @@ const login = async (req, res = response) => {
         })
     }
 
+}
+
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROL'
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
     
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
